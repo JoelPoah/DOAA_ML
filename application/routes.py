4f9e9e23ae3beb0c45104993a5bd11ab2c03938d
Joel <2b11.2112729.joelpoah@gmail.com>
2022-11-13 12:19:05 +0800

    finish forms ui backend not done yet

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,10 @@
 from application import app
 from flask import render_template
+from application.forms import PredictionForm
 #Handles http://127.0.0.1:5000/hello
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index_page():
-    return render_template('index.html')
+    form = PredictionForm()
+    return render_template('index.html',form=form, title='Stroke Prediction')
