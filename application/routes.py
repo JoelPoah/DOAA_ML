4e267aade54f25c9b1ec0b0af7f0b0c05372ce9b
Joel <2b11.2112729.joelpoah@gmail.com>
2022-11-13 11:02:36 +0800

    created flask template ready to branch out for form ui

---
+++ application/routes.py
@@ -0,0 +1,8 @@
+from application import app
+from flask import render_template
+#Handles http://127.0.0.1:5000/hello
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index_page():
+    return render_template('index.html')
