847ad3090d4447b69b380d8a19618d6395e24159
Joel <2b11.2112729.joelpoah@gmail.com>
2022-11-14 18:15:52 +0800

    back end model prediction working data base not done

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import pickle
 #create the Flask app
 app = Flask(__name__)
 # load configuration from config.cfg
 app.config.from_pyfile('config.cfg')
 #run the file routes.py
+
+
+#AI model file
+joblib_file = "./AI/RFCmodel.pkl"
+# Load from file
+with open(joblib_file, 'rb') as f:
+    ai_model = pickle.load(f)
+
+
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,57 @@
 from application import app
-from flask import render_template
+from flask import render_template,request,flash
+from application import ai_model
 from application.forms import PredictionForm
+import logging
+import sys 
 #Handles http://127.0.0.1:5000/hello
+end_type = ['No Stroke you gonna live','You are gonna have  Stroke you gonna die',]
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index_page():
     form = PredictionForm()
+
     return render_template('index.html',form=form, title='Stroke Prediction')
+@app.route("/predict", methods=['GET','POST'])
+def predict():
+    form = PredictionForm()
+    # if request.method == 'POST':
+        # if form.validate_on_submit():
+    gender_male = form.gender.data
+    hypertension = form.hypertension.data
+    heartdisease = form.heartdisease.data
+    married = form.married.data
+    urban = form.urban.data
+    smoking = form.smoking.data
+    smokeformerly = form.smokeformerly.data
+    govtjob = form.govtjob.data
+    workedbefore = form.workedbefore.data
+    privatework = form.privatework.data
+    selfemployed = form.selfemployed.data
+    workchildren = form.workchildren.data
+    name = form.Name.data
+    age = form.age.data
+    average_glucose_level = form.average_glucose_level.data
+    bmi = form.bmi.data
+    X = [[gender_male,hypertension,heartdisease,married,urban,smokeformerly,smoking,govtjob,workedbefore,privatework,selfemployed,workchildren,age,average_glucose_level,bmi]]
+    # sepal_l = form.sepal_l.da ta
+    # sepal_w = form.sepal_w.data
+    # petal_l = form.petal_l.data
+    # petal_w = form.petal_w.data
+    # X = [[ gender_male]
+    
+    result = ai_model.predict(X)
+    # new_entry = Entry(sepal_length=sepal_l,
+    # sepal_width=sepal_w,
+    # petal_length=petal_l,
+    # petal_width=petal_w,
+    # prediction=int(result[0]),
+    # predicted_on=datetime.utcnow())
+    # add_entry(new_entry)
+    flash(f"Prediction: {end_type[result[0]]}","success")
+# else:
+#     flash("Error, cannot proceed with prediction","danger")
+    return render_template("index.html", title="Enter Iris Parameters", form=form, index=True )
+
+
